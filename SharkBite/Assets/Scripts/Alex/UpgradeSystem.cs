d00a0d3ca5ec8ccf147d6801fec2165bc38a0653
Alehander <deralehander@gmail.com>
2025-06-18 15:44:47 +0200

    Upgrade system

--- SharkBite/Assets/Scripts/Alex/UpgradeSystem.cs
+++ SharkBite/Assets/Scripts/Alex/UpgradeSystem.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class UpgradeSystem : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] 
     private List<Button> button = new List<Button>();
+    private UnityAction[] upgradeMethods;
+
+
+
+    private List<Button> _existingButtons = new List<Button>();
     //public delegate void UpgradeButtonDelegate();
     //public UpgradeButtonDelegate Upgrade;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Button actionButton = Instantiate(button,transform);
-        //actionButton.onClick.AddListener(Upgrade1);
+        upgradeMethods = new UnityAction[] {Upgrade0, Upgrade1 };
+        OnLevelUp();
     }
 
 
@@ -22,15 +28,46 @@ public class UpgradeSystem : MonoBehaviour
     /// </summary>
     public void OnLevelUp() 
     {
-
+        HashSet<int> usedIndexes = new HashSet<int>();
+        if (button.Count < 2)
+        {
+            Debug.LogError("Not enough buttons to select 2 unique upgrades.");
+            return;
+        }
+        for (int i = 0; i < 2;) 
+        {
+            int index = Random.Range(0, button.Count);
+            if (!usedIndexes.Contains(index))
+            {
+                usedIndexes.Add(index);
+                InstantiateButton(index);
+                Debug.Log("nomegwebubgew");
+                i++;
+            }
+        }
     }
 
-    public void Upgrade1()
+    private void InstantiateButton(int index)
+    {
+        Button actionButton = Instantiate(button[index], transform, this.transform);
+        actionButton.onClick.AddListener(upgradeMethods[index]);
+        actionButton.onClick.AddListener(DestroyExistingButtons);
+        _existingButtons.Add(actionButton);
+    }
+
+    private void DestroyExistingButtons()
+    {
+        foreach (Button button in _existingButtons)
+            Destroy(button.gameObject);
+        _existingButtons.Clear();
+    }
+
+    public void Upgrade0()
     {
         Debug.Log("I WORK");
     }
 
-    public void Upgrade2()
+    public void Upgrade1()
     {
         Debug.Log("I WORK 2");
     }
