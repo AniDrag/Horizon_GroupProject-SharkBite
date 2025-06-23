7f29f9653be59d7cdcdaef0f85737e3f13841356
AniDrag <nikecoblak@gmail.com>
2025-06-23 14:29:12 +0200

    Shoot mechanic on player

--- SharkBite/Assets/Scripts/Alex/Weapon.cs
+++ SharkBite/Assets/Scripts/Alex/Weapon.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
@@ -6,10 +7,13 @@ public class Weapon : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform orientation;
 
-    private PlayerStats playerStats;
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private PlayerStats playerStats;
     private float _timeReset;
+    Vector3 _saveShoodDirection;
     void Start()
     {
+        _playerInput = GetComponent<PlayerInput>();
         playerStats = PlayerManager.instance.playerStats;
         if (projectilePrefab.GetComponent<Rigidbody>() == null)
         {
@@ -22,15 +26,22 @@ public class Weapon : MonoBehaviour
     {
         _timeReset += Time.deltaTime;
 
-        
+        Vector2 _moveDirection = _playerInput.actions["Look"].ReadValue<Vector2>();
+        Vector3 _3dMoveDirection = new Vector3(_moveDirection.x, 0, _moveDirection.y);
+        if (_3dMoveDirection != Vector3.zero)
+        {
+            _saveShoodDirection = _3dMoveDirection;
+            orientation.rotation = Quaternion.LookRotation(_saveShoodDirection);
+        }
+
     }
     private void FixedUpdate()
     {
         if (_timeReset >= playerStats.GetFireRate())
         {
+            _timeReset = 0;
             // Debug.Log("I shot a bullet");
             Shoot(playerStats.GetFireRate(), orientation.position);
-            _timeReset = 0;
         }
     }
 
