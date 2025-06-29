e0a803f412adfec9ce7637da974a694de704365e
Alehander <deralehander@gmail.com>
2025-06-29 13:23:42 +0200

    Platoon

---
+++ SharkBite/Assets/Scripts/Alex/PlatoonShooting.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlatoonShooting : MonoBehaviour
+{
+
+
+    private float _timeReset;
+    private PlayerStats _playerStats;
+    private Pooler _itemPooler;
+    void Start()
+    {
+        _playerStats = PlayerManager.instance.playerStats;
+        _itemPooler = Pooler.instance;
+    }
+
+    private void FixedUpdate()
+    {
+        _timeReset += Time.deltaTime;
+        if (_timeReset >= _playerStats.GetFireRate())
+        {
+            _timeReset = 0;
+
+            Shoot(_playerStats.GetFireRate());
+        }
+    }
+
+    private void Shoot(float cd)
+    {
+        GameObject newBullet = _itemPooler.SpawnFromPool("Bullet", transform.position, Quaternion.identity);
+        newBullet.GetComponent<Damage>().SetDamage(_playerStats.GetBulletDamage());
+
+        Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            Vector3 direction = transform.position - GameManager.instance._playerPos;
+            rb.AddForce(direction * _playerStats.GetBulletSpeed() * 200, ForceMode.Force);
+        }
+    }
+
+
+}
